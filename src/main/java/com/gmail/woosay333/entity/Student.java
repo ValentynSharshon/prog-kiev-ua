cb3dc38aae214f4d2fa137e9872a2c62dfb64fb6
Valentyn Sharshon <woosay333@gmail.com>
2023-03-04 23:04:58 +0200

    add Student entity class

---
+++ src/main/java/com/gmail/woosay333/entity/Student.java
@@ -0,0 +1,45 @@
+package com.gmail.woosay333.entity;
+
+import com.gmail.woosay333.enums.Gender;
+
+public class Student extends Human {
+
+    private int id;
+    private String groupName;
+
+    public Student() {
+    }
+
+    public Student(String name, String lastName, Gender gender, int id, String groupName) {
+        super(name, lastName, gender);
+        this.id = id;
+        this.groupName = groupName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student {" +
+                "name = " + getName() +
+                ", lastName = " + getLastName() +
+                ", id = " + id +
+                ", groupName = '" + groupName + '\'' +
+                '}';
+    }
+
+}
