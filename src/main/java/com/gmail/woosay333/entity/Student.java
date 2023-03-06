b92b2195dc332b9016f63793fca2fff49e940cc5
Valentyn Sharshon <woosay333@gmail.com>
2023-03-06 13:16:52 +0200

    implement CSVConvertor interface

--- src/main/java/com/gmail/woosay333/entity/Student.java
+++ src/main/java/com/gmail/woosay333/entity/Student.java
@@ -2,7 +2,9 @@ package com.gmail.woosay333.entity;
 
 import com.gmail.woosay333.enums.Gender;
 
-public class Student extends Human {
+import java.util.StringJoiner;
+
+public class Student extends Human implements CSVConverter {
 
     private int id;
     private String groupName = "Unknown";
@@ -30,6 +32,29 @@ public class Student extends Human {
 
     public void setGroupName(String groupName) {
         this.groupName = groupName;
+    }
+
+    @Override
+    public String toCSVString() {
+        return new StringJoiner(";")
+                .add(getName())
+                .add(getLastName())
+                .add(getGender().toString())
+                .add(String.valueOf(getId()))
+                .add(getGroupName())
+                .toString();
+    }
+
+    @Override
+    public Student fromCSVString(String str) {
+        if (str == null || str.isEmpty()) {
+            throw new IllegalArgumentException("Input string is null or empty!");
+        }
+
+        String[] array = str.split("\\W+");
+        return new Student(array[0], array[1],
+                Gender.valueOf(array[2].toUpperCase().trim()),
+                Integer.parseInt(array[3]));
     }
 
     @Override
