0a4a4274c3d2900a3403d53efecb8000ec54b8f8
Valentyn Sharshon <woosay333@gmail.com>
2023-03-21 13:00:08 +0200

    test encodingDemo method

--- src/main/java/com/gmail/woosay333/files/FilesRunner.java
+++ src/main/java/com/gmail/woosay333/files/FilesRunner.java
@@ -2,12 +2,13 @@ package com.gmail.woosay333.files;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
 
 import static java.lang.System.out;
 
 public class FilesRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         FileFilter fileFilter = new MyFileFilter("mp3", "exe", "zip", "json");
         String path = "E:\\Dow";
         File folder = new File(path);
@@ -20,6 +21,8 @@ public class FilesRunner {
 
         long size = FileOperations.getFolderSize(folder);
         out.println(size);
+
+        FileOperations.encodingDemo("C:\\Users\\Администратор\\Desktop\\нові паролі для ЕЦП ключів.txt");
     }
 
 }
