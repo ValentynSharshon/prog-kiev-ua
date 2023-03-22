4d5f91fa1cde8afe05421e6702bb480cfb138a1c
Valentyn Sharshon <woosay333@gmail.com>
2023-03-22 13:13:54 +0200

    add transferDemo and readLinesFromFile methods

--- src/main/java/com/gmail/woosay333/nio/ChannelExample.java
+++ src/main/java/com/gmail/woosay333/nio/ChannelExample.java
@@ -5,13 +5,43 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import static java.lang.System.out;
 
 public class ChannelExample {
     public static void main(String[] args) {
         String from = "E:\\Dow\\Windows\\ru windows xp professional with sp 3 x86 cd vl x14-74146.iso";
         String to = "copy.iso";
 
-        copyFile(from, to);
+        //copyFile(from, to);
+        //transferDemo(from, to);
+        readLinesFromFile(".gitignore");
+    }
+
+    public static void readLinesFromFile(String filePath) {
+        Path path = Path.of(filePath);
+        try (Stream<String> lines = Files.lines(path, StandardCharsets.UTF_8)) {
+            lines.forEach(out::println);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void transferDemo(String from, String to) {
+        try (FileInputStream inputStream = new FileInputStream(from);
+             FileOutputStream outputStream = new FileOutputStream(to)) {
+
+            FileChannel inputChannel = inputStream.getChannel();
+            FileChannel outputChannel = outputStream.getChannel();
+
+            inputChannel.transferTo(0, inputChannel.size(), outputChannel);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public static void copyFile(String from, String to) {
