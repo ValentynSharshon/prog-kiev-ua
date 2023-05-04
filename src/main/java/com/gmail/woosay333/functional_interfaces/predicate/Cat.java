bcdff41ba7bd7a483e9bd134fb2cf4a3e9d06fb7
Valentyn Sharshon <woosay333@gmail.com>
2023-05-04 12:47:58 +0300

    add equals and compareTo methods

--- src/main/java/com/gmail/woosay333/functional_interfaces/predicate/Cat.java
+++ src/main/java/com/gmail/woosay333/functional_interfaces/predicate/Cat.java
@@ -1,6 +1,8 @@
 package com.gmail.woosay333.functional_interfaces.predicate;
 
-public class Cat {
+import java.util.Objects;
+
+public class Cat implements Comparable<Cat> {
 
     private String name;
     private int age;
@@ -29,6 +31,47 @@ public class Cat {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Cat other = (Cat) obj;
+
+        return age == other.getAge() && Objects.equals(name, other.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public int compareTo(Cat other) {
+        if (other == null) {
+            throw new NullPointerException();
+        }
+
+        if (age > other.age) {
+            return 1;
+        }
+
+        if (age < other.age) {
+            return -1;
+        }
+
+        return name.compareTo(other.name);
     }
 
     @Override
